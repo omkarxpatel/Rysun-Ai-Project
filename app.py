4a247b970bd542dfbaf75bd15044796ee5a17218
Omkar Patel <omkar.patel@rysun.com>
2024-06-18 17:29:20 -0700

    v2

--- app.py
+++ app.py
@@ -1,55 +1,102 @@
 from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+from meta_ai_api import MetaAI
 from flask_cors import CORS
-import time
+from openai import OpenAI
+from bardapi import Bard
+import time, os
 
+load_dotenv()
 app = Flask(__name__)
+
+chatgpt_client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+# gemini_client = Bard(
+    # token=os.environ.get("BARD_API_KEY")
+# )
+meta_client = MetaAI()
+
 CORS(app)
 
-## GLOBAL VALUES
+current_model = ""
+data = {'model': current_model}
 
-current_models = [] 
-data = {'models':current_models}
-# array because multiple can be used at once
-# in case with more than 1 model, use chatgpt to summarize all outputs or just display all at once
+@app.route('/api/status', methods=['GET']) ## status of api. gets pinged every 5s
+def get_status():
+    data['status'] = 'ok'
+    return jsonify(data), 200
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data["message"] = "Hello"
-    return jsonify(data)
+@app.route('/api/prompt-submission', methods=['POST']) ## handles when a user submits a prompt, no matter which ai they choose
+def handle_prompt_submission():
+    data = request.get_json()
+    prompt = data.get('prompt')
+    model = data.get('model')
+    result = ''
+    print(model)
+
+    print(f"Prompt received: {prompt}")
+
+    if not model:
+        result = "Please select a model"
+        
+    elif model == "ChatGPT":
+        try:
+            completion = chatgpt_client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": prompt},
+                ]
+            )
+
+            result = completion.choices[0].message.content
+            print(result)
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": str(e)}), 500
+        
+    # elif model == "Bard":
+    #     try:
+            # completion = gemini_client.generate_content(prompt)
+            # result = completion.payload
+    #         print(result)
+            
+    #     except Exception as e:
+    #         print(f"Error: {e}")
+    #         return jsonify({"error": str(e)}), 500
+        
+    elif model == "Meta AI":
+        try:
+            result = meta_client.prompt(message=prompt)['message']
+            print(result)
+            
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": str(e)}), 500
+
+    else:
+        result = "Current model not supported"
+
+    output = {"response": result, "prompt": prompt}
+    return jsonify(output)
 
 
-@app.route('/api/button-clicked', methods=['POST'])
-def handle_button_click():
-    button_data = request.json
-    button_name = button_data.get('buttonName')
-    print(f'Button "{button_name}" has been clicked.')
-
-    data['message'] = f'Handled button click for "{button_name}"'
-    return jsonify(data)
-
-
-"""
-Used for the AI model selection
-
-gathers inputs and adds the users models to an array; current_models
-"""
-@app.route('/api/dropdown-selected', methods=['POST'])
+@app.route('/api/model-dropdown-selected', methods=['POST'])
 def handle_model_dropdown_selected():
     selected_data = request.json
     selected_option = selected_data.get('selectedOption')
     print(f'Dropdown option "{selected_option}" has been selected.')
-    
-    print(data['models'])
-    data['model_selection_message'] = f'Selected models: "{data['models']}"'
 
-    if selected_option not in data['models']:
-        data['models'].append(selected_option)
-    else:
-        data['models'].remove(selected_option)
+    print(data['model'])
+    data['model_selection_message'] = f'Selected model: "{data['model']}"'
+
+    data['model'] = selected_option
+    # if selected_option not in data['model']:
+        # data['model'].append(selected_option)
+    # else:
+        # data['model'].remove(selected_option)
 
     return jsonify(data)
-
-
 
 def run_app_with_retries():
     retry_interval = 5
@@ -59,8 +106,5 @@ def run_app_with_retries():
         except Exception as e:
             print(f"Server error: {e}. Restarting in {retry_interval} seconds...")
             time.sleep(retry_interval)
-            
 
-if __name__ == '__main__':
-    # app.run(debug=True)
-    run_app_with_retries()
+run_app_with_retries()
